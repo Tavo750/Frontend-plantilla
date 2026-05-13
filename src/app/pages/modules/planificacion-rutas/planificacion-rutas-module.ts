4a1fc4d14b88066adea8f60172d6320dbeaf3809
Gustavo <a20215847@pucp.edu.pe>
2026-05-13 00:54:49 -0500

    feat: add modules and routing for Dashboard, MonitoreoMapa, PlanificacionRutas, and RegistroMaletas

---
+++ src/app/pages/modules/planificacion-rutas/planificacion-rutas-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlanificacionRutasRoutingModule } from './planificacion-rutas-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PlanificacionRutasRoutingModule],
+})
+export class PlanificacionRutasModule {}
---
+++ src/app/pages/modules/registro-maletas/registro-maletas-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegistroMaletasRoutingModule } from './registro-maletas-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RegistroMaletasRoutingModule],
+})
+export class RegistroMaletasModule {}
