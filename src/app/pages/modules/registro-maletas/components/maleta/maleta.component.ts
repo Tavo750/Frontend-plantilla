fbe4b097f57e5a04482f256493050b1ffffb4ab0
Gustavo <a20215847@pucp.edu.pe>
2026-05-13 01:31:46 -0500

    feat: add MaletaComponent with template and styles, and update RegistroMaletasModule and routing

---
+++ src/app/pages/modules/registro-maletas/components/maleta/maleta.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maleta',
+  standalone: false,
+  templateUrl: './maleta.component.html',
+  styleUrl: './maleta.component.css',
+})
+export class MaletaComponent {}
--- src/app/pages/modules/registro-maletas/registro-maletas-module.ts
+++ src/app/pages/modules/registro-maletas/registro-maletas-module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegistroMaletasRoutingModule } from './registro-maletas-routing-module';
+import { MaletaComponent } from './components/maleta/maleta.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, RegistroMaletasRoutingModule],
+  declarations: [
+    MaletaComponent,
+
+  ],
+  imports: [
+    CommonModule, 
+    RegistroMaletasRoutingModule
+  ],
 })
-export class RegistroMaletasModule {}
+export class RegistroMaletasModule {
+
+}
