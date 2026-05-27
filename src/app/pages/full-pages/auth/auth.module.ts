072a2a540294c6eaa77565576664efd3897015df
Tavo750 <a20215847@pucp.edu.pe>
2026-05-27 12:50:46 -0500

    feat: implement auth module and increase build budget thresholds in angular.json

--- src/app/pages/full-pages/auth/auth.module.ts
+++ src/app/pages/full-pages/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing-module';
+import { AuthRoutingModule } from './auth-routing.module';
 import { Login } from './components/login/login';
 import { CrearUsuario } from './components/crear-usuario/crear-usuario';
 
