e97b69ba51a9fee2635011559c88caeaf7fde441
Tavo750 <a20215847@pucp.edu.pe>
2026-05-27 12:39:44 -0500

    feat: add authentication module with login and register components

---
+++ src/app/pages/full-pages/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing-module';
+import { Login } from './components/login/login';
+import { CrearUsuario } from './components/crear-usuario/crear-usuario';
+
+@NgModule({
+  declarations: [Login, CrearUsuario],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
---
+++ src/app/pages/full-pages/auth/components/crear-usuario/crear-usuario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-usuario',
+  standalone: false,
+  templateUrl: './crear-usuario.html',
+  styleUrl: './crear-usuario.css',
+})
+export class CrearUsuario {}
