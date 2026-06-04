f59dd3d1e78e59a3c456028b0be37c97b0422417
Tavo750 <a20215847@pucp.edu.pe>
2026-06-04 17:26:56 -0500

    feat: add production environment configuration file

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
   // Nginx escucha en el puerto 80 y redirige el tráfico de /api/*
   // hacia el backend interno en http://127.0.0.1:8080/.
   // De esta forma nunca se expone el puerto 8080 al exterior.
-  apiUrl: '/api/',       // Peticiones generales  → Nginx → :8080/api/
+  apiUrl: 'http://backend7g-tasfb2bbackend-fvm1gb-41d9b0-200-16-7-153.sslip.io/api/',       // Peticiones generales  → Nginx → :8080/api/
   authUrl: '/api/auth/', // Peticiones de auth     → Nginx → :8080/auth/
 };
