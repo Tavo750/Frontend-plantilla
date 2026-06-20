731f2c52013e8696cfe59d887b9b94f914cc46ce
Tavo750 <a20215847@pucp.edu.pe>
2026-06-20 11:20:09 -0500

    fix: revert API and Auth URLs to use HTTP for production environment

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
   // a través del dominio generado por Dokploy (sslip.io).
   // El navegador hace las peticiones directamente al backend; Nginx solo
   // sirve los archivos estáticos de Angular (SPA).
-  apiUrl:  'https://backend7g-tasfb2bbackend-fvm1gb-41d9b0-200-16-7-153.sslip.io/api/',
-  authUrl: 'https://frontend7g-tasfb2bfrontend-fvm1gb-41d9b0-200-16-7-153.sslip.io/auth',
+  apiUrl:  'http://backend7g-tasfb2bbackend-fvm1gb-41d9b0-200-16-7-153.sslip.io/api/',
+  authUrl: 'http://frontend7g-tasfb2bfrontend-fvm1gb-41d9b0-200-16-7-153.sslip.io/auth',
 };
